ce6afebdd05bd479c4784a2088112ee04efe148b
津野亮太 <a_bc_j1024@icloud.com>
2018-09-21 18:56:42 +0900

    front実装
    chatspaceのfrontを実装した

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+    devise_for :users
+
+    root 'messages#index'
+    resources :users, only: [:index, :edit, :update]
+    resources :groups, only: [:new, :create, :edit, :update] do
+        resources :messages, only: [:create, :index]
+    end
 end
