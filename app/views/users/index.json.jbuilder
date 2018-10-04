b71a30d2eddaa09156a9f4ce72305092cdf5f500
津野亮太 <a_bc_j1024@icloud.com>
2018-10-04 15:52:29 +0900

    controllerとjbuilder
    コントローラーのindexアクションと、jbuilderを設定した

---
+++ app/views/users/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.arry! @users, :id, :name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180926094754) do
+ActiveRecord::Schema.define(version: 20181004053254) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180926094754) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
