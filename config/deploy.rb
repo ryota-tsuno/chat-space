57f8aac9bdb916d51f36a965d305b8a9c2dc2acc
津野亮太 <a_bc_j1024@icloud.com>
2018-10-11 18:32:01 +0900

    Modify_deploy.rb
    修正しました

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,11 +23,6 @@ namespace :deploy do
   end
 end
 
-lock "~> 3.11.0"
-
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
